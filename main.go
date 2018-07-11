7baf9cdcc9d0d09fa2c353b60b958bd1d7252b2f
Will Lewis <will@wflewis.com>
2018-07-11 17:16:26 -0400

    Add initial postgres connection

--- main.go
+++ main.go
@@ -3,14 +3,29 @@ package main
 import (
   "github.com/aws/aws-lambda-go/events"
   "github.com/aws/aws-lambda-go/lambda"
+  "github.com/go-pg/pg"
+  "github.com/go-pg/pg/orm"
+  "fmt"
   "log"
   "os"
   "net/http"
+  "errors"
 )
 
 const (
   AWSLambdaFunctionVersion = "AWS_LAMBDA_FUNCTION_VERSION"
+  PostgresUrl = "POSTGRES_URL"
 )
+
+type PageView struct {
+    Id         int64
+    Referer    string
+    RemoteAddr string
+}
+
+func (u PageView) String() string {
+    return fmt.Sprintf("PageView<%d %s %s>", u.Id, u.Referer, u.RemoteAddr)
+}
 
 func main() {
   _, ok := os.LookupEnv(AWSLambdaFunctionVersion)
@@ -23,6 +38,29 @@ func main() {
   log.Printf("Not running in AWS lambda environment, starting mock handler.")
   MockHandler()
   os.Exit(0)
+}
+
+func SetupDB() {
+  url, present := os.LookupEnv(PostgresUrl)
+  if !present {
+    panic(errors.New(fmt.Sprintf("missing env %s", PostgresUrl)))
+  }
+
+  options, err := pg.ParseURL(url)
+  if err != nil {
+    panic(err)
+  }
+
+  db := pg.Connect(options)
+  defer db.Close()
+
+  for _, model := range []interface{}{&PageView{}} {
+    err = db.CreateTable(model, &orm.CreateTableOptions{})
+
+    if err != nil {
+      panic(err)
+    }
+  }
 }
 
 func AWSHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
