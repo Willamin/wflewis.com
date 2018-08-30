604cbd7d62ce52aef95944f0726ec57bf81f1ea3
Will Lewis <will@wflewis.com>
2018-08-30 00:29:42 -0400

    Update to add blog

--- config.rb
+++ config.rb
@@ -4,3 +4,19 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
 end
+
+page "/", :layout => "landing"
+
+activate :blog do |blog|
+  blog.prefix = "blog"
+  blog.permalink = "{title}"
+  blog.generate_day_pages = false
+  blog.generate_month_pages = false
+  blog.generate_tag_pages = false
+  blog.generate_year_pages = false
+  blog.layout = "post"
+  blog.sources = "{id}-{title}.html"
+  blog.default_extension = "md"
+end
+
+activate :directory_indexes
---
+++ source/blog/01-hello-world.html.md
@@ -0,0 +1,6 @@
+---
+title: Hello World!
+date: 2018-08-30
+---
+## or how I learned to love the blog
+Just a quick blog post to test that this works.
